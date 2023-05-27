46f647ee10519e1a9f6f536ff44f51bb314783e7
HangerIsACat <sheenadale.mariano@gmail.com>
2023-05-28 02:39:45 +0800

    Add functionalities for update() and delete() in BooksDBloader.js

--- src/dao/BooksDBloader.js
+++ src/dao/BooksDBloader.js
@@ -1,9 +1,11 @@
 
 import Database from "better-sqlite3";
+
 import { Book } from "./../entities/Book.js";
+import { Message } from "../util/Message.js";
+import { Utility } from "../util/Utility.js";
 
 import booksJSON from "../data/books.json" assert {type: "json"};
-import { Message } from "../util/Message.js";
 
 export class BooksDBloader {
 
@@ -38,19 +40,70 @@ export class BooksDBloader {
 
   get(bookID) {
     const sql = this.db.prepare(`
-      SELECT id, title, location 
-      FROM books WHERE id = ?;
+      SELECT id, title, location FROM books WHERE id = ?;
     `);
-    
-    return sql.run(bookID);;
+
+    const book = sql.get(bookID);
+
+    if (Utility.checkEmpty(book)) {
+      return Message.ERROR_BOOK_ID_NOT_EXIST;
+    }
+
+    return book;
   }
 
   update(bookUpdated) {
     
+    console.log(bookUpdated);
+
+    const bookForUpdating = this.get(bookUpdated.id);
+
+    if (Utility.checkEmpty(bookForUpdating)) {
+      return Message.ERROR_BOOK_ID_NOT_EXIST;
+    }
+    
+    if (bookUpdated && bookUpdated.title) {
+      bookForUpdating.title = bookUpdated.title;
+    }
+
+    if (bookUpdated && bookUpdated.location) {
+      bookForUpdating.location = bookUpdated.location;
+    }
+
+    const sql = this.db.prepare(`
+      UPDATE books SET title = ?, location = ? 
+      WHERE id = ?;
+    `);
+
+    let result = sql.run(
+      bookForUpdating.title, 
+      bookForUpdating.location, 
+      bookForUpdating.id);
+
+    if (result.changes > 0) {
+      return Message.SUCCESS;
+
+    } else {
+      return Message.ERROR_BOOK_ID_NOT_EXIST;
+    }
+
   }
 
   delete(bookID) {
-    
+
+    const sql = this.db.prepare(`
+      DELETE from books WHERE id = ?;
+    `);
+
+    let result = sql.run(bookID);
+
+    if (result.changes > 0) {
+      return Message.SUCCESS;
+
+    } else {
+      return Message.ERROR_BOOK_ID_NOT_EXIST;
+    }
+
   }
 
   add(book) {
@@ -59,9 +112,15 @@ export class BooksDBloader {
       VALUES(?, ?);
     `);
 
-    sql.run(book.title, book.location);
+    let result = sql.run(book.title, book.location);
 
-    return Message.SUCCESS;
+    if (result.changes > 0) {
+      return Message.SUCCESS;
+
+    } else {
+      return Message.ERROR_ON_INSERT;
+    }
+
   }
 
 }
