6f27cffdbec62c2672039079d7aa89190b0fb054
HangerIsACat <sheenadale.mariano@gmail.com>
2023-05-28 02:37:59 +0800

    Add new message for insert error in Message.js

--- src/util/Message.js
+++ src/util/Message.js
@@ -6,5 +6,6 @@ export class Message {
 
   static ERROR_BOOK_NULL = "Book passed is null.";
   static ERROR_BOOK_ID_NOT_EXIST = "Book ID doesn't exist.";
+  static ERROR_ON_INSERT = "Error encountered while creating new book in DB";
 
 }
