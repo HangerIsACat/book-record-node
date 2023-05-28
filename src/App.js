bdd4affa3574909bad4debf434f93347e046fe06
HangerIsACat <sheenadale.mariano@gmail.com>
2023-05-28 20:07:29 +0800

    Initial commit for locations endpoint

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+
 import { Book } from "./entities/Book.js";
 import { BookService } from "./services/BookService.js";
+import { Location } from "./entities/Location.js"
+import { LocationService } from "./services/LocationService.js"
 
 const PORT = 3001;
 
@@ -13,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const bookService = new BookService();
+const locationService = new LocationService();
 
 app.get("/books", (req, res) => {
   const books = bookService.getBooks();
@@ -20,27 +24,21 @@ app.get("/books", (req, res) => {
 });
 
 app.get("/book/:id", (req, res) => {
-  let bookID = req.params.id;
-
-  const books = bookService.get(bookID);
+  const books = bookService.get(req.params.id);
   console.log(books);
   
   res.send(books);
 });
 
 app.get("/book/:id", (req, res) => {
-  let bookID = req.params.id;
-
-  const book = bookService.get(bookID);
+  const book = bookService.get(req.params.id);
   res.send(book);
 });
 
 app.post("/book/:id", (req, res) => {
-  let bookID = req.params.id;
-
   const message = bookService.update(
     new Book(
-      bookID, 
+      req.params.id, 
       req.body.title, 
       req.body.location));
 
@@ -48,9 +46,7 @@ app.post("/book/:id", (req, res) => {
 });
 
 app.delete("/book/:id", (req, res) => {
-  let bookID = req.params.id;
-
-  const message = bookService.delete(bookID);
+  const message = bookService.delete(req.params.id);
   res.send(message);
 });
 
@@ -64,6 +60,42 @@ app.post("/book", (req, res) => {
   res.send(message);
 });
 
+app.get("/locations", (req, res) => {
+  const locations = locationService.getLocations();
+  res.send(locations);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running @ port ${PORT}`);
+});
+
+app.get("/location/:id", (req, res) => {
+  const location = locationService.get(req.params.id);
+  res.send(location);
+});
+
+app.post("/location/:id", (req, res) => {
+  const message = locationService.update(
+    new Location(
+      req.params.id, 
+      req.body.name, 
+      parseInt(req.body.parentID)
+    ));
+
+  res.send(message);
+});
+
+app.delete("/location/:id", (req, res) => {
+  const message = locationService.delete(req.params.id);
+  res.send(message);
+});
+
+app.post("/location", (req, res) => {
+  const location  = new Location(
+    parseInt(req.body.id), 
+    req.body.name, 
+    req.body.parentID);
+
+  const message = locationService.add(location);
+  res.send(message);
 });
---
+++ src/entities/Location.js
@@ -0,0 +1,10 @@
+
+export class Location {
+
+  constructor(id, name, parentID) {
+    this.id = id;
+    this.name = name;
+    this.parentID = parentID;
+  }
+
+}
