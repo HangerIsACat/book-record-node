7cc437576f4c584846ded99a6d6451f91c7472ba
HangerIsACat <sheenadale.mariano@gmail.com>
2023-05-28 20:05:58 +0800

    Add messages for locations endpoint

--- src/util/Message.js
+++ src/util/Message.js
@@ -6,6 +6,10 @@ export class Message {
 
   static ERROR_BOOK_NULL = "Book passed is null.";
   static ERROR_BOOK_ID_NOT_EXIST = "Book ID doesn't exist.";
-  static ERROR_ON_INSERT = "Error encountered while creating new book in DB";
+
+  static ERROR_LOCATION_NULL = "Book passed is null.";
+  static ERROR_LOCATION_ID_NOT_EXIST = "Book ID doesn't exist.";
+
+  static ERROR_ON_INSERT = "Error encountered while creating entity in DB";
 
 }
